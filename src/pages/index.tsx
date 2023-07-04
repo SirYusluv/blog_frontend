2687eb0e5e12dfe3be90a637b94072a96f8b95d0
SirYusluv <olanrewajuyusuf379@gmail.com>
2023-07-04 13:08:36 +0100

    implemented add botton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,33 @@
 import Head from "next/head";
+import add from "@/imgs/add.svg";
+import Image from "next/image";
+import classes from "./index.module.scss";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
     <>
       <Head>
         <title>Blog</title>
         <meta name="description" content="Blog list" />
       </Head>
-      <p>No blog yet</p>
+      <main className="blogs">
+        <div className={`${classes["blogs__no-blog"]} center info`}>
+          <p>Nothing here yet!!!</p>
+          <p>Click the add "+" botton below to start creating blog posts.</p>
+        </div>
+
+        <Image
+          className={`${classes.add} link`}
+          alt="add button"
+          src={add}
+          width={50}
+          height={50}
+          onClick={() => router.push("/add")}
+        />
+      </main>
     </>
   );
 }
